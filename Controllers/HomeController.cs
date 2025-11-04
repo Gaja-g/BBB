e4022ccdebc87b06c068db716c5fb62bc1d7911f
Filip Stranovsky - SDU <fistr24@student.sdu.dk>
2025-11-04 19:30:18 +0100

    Fix

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -65,18 +65,6 @@ public class HomeController : Controller
         return View();
     }
     // Testing the db queries
-    public async Task<IActionResult> Test()
-    {
-        var foo = await _db.BoardGameUsers.Where(x => x.UserId == 4).ToListAsync();
-        var lines = foo.Select(x => $"{x.BoardGame.Title} - Borrowed on: {x.BorrowDate:yyyy-MM-dd} - Returned on: {x.ReturnDate:yyyy-MM-dd}");
-        return Content(string.Join("\n", lines), "text/plain");
-
-        /*
-        var usernames = await _db.BoardGames.Where(x => x.Title == "Catan").FirstAsync();
-        var usr = usernames.BoardGameTags.Select(x => x.Tag.Name);
-        return Content(string.Join("\n", usr), "text/plain");
-        */
-    }
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
