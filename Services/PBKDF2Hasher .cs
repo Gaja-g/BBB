e21dc22491c7a1c14dc7f82b9696d02fbb861016
Filip Stranovsky - SDU <fistr24@student.sdu.dk>
2025-11-06 08:45:58 +0100

    Commit with a db passwords hashed

--- Services/PBKDF2Hasher .cs
+++ Services/PBKDF2Hasher .cs
@@ -8,8 +8,9 @@ public static class PBKDF2Hasher
     public static byte[] Hash(string password, byte[] salt) =>
         Rfc2898DeriveBytes.Pbkdf2(password, salt, 100000, HashAlgorithmName.SHA256, 32);
 
-    public static bool Verify(string password, string saltString, string hashString)
+    public static bool Verify(string password, string hashString, string saltString)
     {
+        Debug.WriteLine(hashString);
         byte[] hash = Convert.FromBase64String(hashString);
         byte[] salt = Convert.FromBase64String(saltString);
 
